2514ae569cfbe5175993c28d79f66f278f67835e
Ela Wróbel <elzbieta.wrobel@capgemini.com>
2020-10-28 18:31:40 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 
-const News = () => <p>Hello !</p>;
+// const News = () => <p>Hello !</p>;
 
-// function News() {
-//   return(
-//     <p>Hello!</p>
-//   );
-// }
+function News( { h2, p } ) { // function News( props ) {
+
+  // const { h2, p } = props;
+
+  return(
+    <div>
+      <h2>{ h2 }</h2>
+      <p>{ p }</p>
+    </div>
+  );
+}
 
 function App() {
   return (
