fd45c3b7093e6a953a2d924492361bb202298bc0
Ela Wróbel <elzbieta.wrobel@capgemini.com>
2020-10-31 10:08:34 +0100

    style css in js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Fragment } from 'react';
 
 import './App.css';
 
+// CSS in JS
+const red = 'f53b57';
+
+const styles = {
+  backgroundColor: '#808e9b',
+  border: '1px solid black',
+  padding: 8,
+  color: '#d2dae2',
+  para: {
+    color: red
+  }
+}
 
 function News( { header, intro } ) {
   return(
     <div className="news">
       <h2>{ header }</h2>
-      <p>{ intro }</p>
+      <p style={styles}>{ intro }</p>
+      <p style={styles.para}>{ intro }</p>
     </div>
   );
 }
