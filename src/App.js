0b747fa50da0c7f923b3bdfb3da1e9d5e4be2ec6
Ela Wróbel <elzbieta.wrobel@capgemini.com>
2020-10-31 09:48:32 +0100

    data.map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import './App.css';
 
-const DAY = 'monday';
-
 // const News = () => <p>Hello !</p>;
 function News( { header, intro } ) { // function News( props ) {
-
   // const { header, intro } = props;
-
   return(
     <div>
       <h2>{ header }</h2>
@@ -17,27 +13,25 @@ function News( { header, intro } ) { // function News( props ) {
   );
 }
 
+const data = [ 
+  {
+    header: 'Nagłówek 1', intro: 'Intro 1',
+  }, {
+    header: 'Nagłówek 2', intro: 'Intro 2',
+  }, { 
+    header: 'Nagłówek 3', intro: 'Intro 3',
+  } 
+];
+
+// data.map( (elem, index) )
+
 function App() {
-
-  const checkDay = () => {
-    if( DAY === 'monday' ){
-      return(
-        <div>
-          <News header="Nagłówek 1" intro="Intro 1" />
-          <News header="Nagłówek 2" intro="Intro 2" />
-          <News header="Nagłówek 3" intro="Intro 3" />
-        </div>
-      );
-    }
-    return(
-      <p>nie ma newsow</p>
-    )
-  }
-
-  return (
-    <div className="App">
-      {checkDay()}
-    </div>
+  return(
+    <Fragment>
+      { data.map( (elem, index) => {
+        return <News header={elem.header} intro={elem.intro} />;
+      }) }
+    </Fragment>
   );
 }
 
