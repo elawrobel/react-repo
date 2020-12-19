a240acdf87fca0c9eeea203080d61b2471c5bc41
Ela Wróbel <elzbieta.wrobel@capgemini.com>
2020-12-19 16:45:00 +0100

    Timmer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-
 const styles = {
   border: '1px solid #808e9b',
   backgroundColor: 'rgb(210 218 226 / 48%)',
@@ -9,9 +8,22 @@ const styles = {
   fontSize: '10px',
 };
 
-
+// Function component
 const Timer = () => {
   const [ count, setCount ] = useState(0);
+
+  // change second into minutes/hours
+  function secondsToHms(d) {
+    d = Number(d);
+    const h = Math.floor(d / 3600);
+    const m = Math.floor(d % 3600 / 60);
+    const s = Math.floor(d % 3600 % 60);
+  
+    const hDisplay = h > 0 ? h + (h === 1 ? ' godzinę, ' : ' godzin, ') : '';
+    const mDisplay = m > 0 ? m + (m === 1 ? ' minutę, ' : ' minut, ') : '';
+    const sDisplay = s > 0 ? s + (s === 1 ? ' sekundę ' : ' sekund') : '';
+    return hDisplay + mDisplay + sDisplay; 
+  }
 
   useEffect(() => {
     window.setInterval(() => {
@@ -19,7 +31,7 @@ const Timer = () => {
     }, 1000);
   }, []);
 
-  return <div style={styles}>Jesteś na tej stronie juz {count} sekund</div>;
+  return <div style={styles}>Jesteś na tej stronie juz {secondsToHms(count)}</div>;
 };
 
 export default Timer;
